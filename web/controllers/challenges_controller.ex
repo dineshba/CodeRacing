a56a53e3cc3fc7bd4c8762587af2bfe40f3084da
Dinesh Balasubramanian <dineshudt17@gmail.com>
2017-11-06 23:52:37 +0530

    [Dinesh] Included time also in challenges

--- web/controllers/challenges_controller.ex
+++ web/controllers/challenges_controller.ex
@@ -13,17 +13,23 @@ defmodule CodeRacing.ChallengesController do
     %{current_challenge: current_player_challenge} = current_player = conn.assigns.current_player
     %{problem_set: problems} = CodeRacing.Challenges.get(current_player_challenge)
     random_problem = problems |> Enum.random
-    CodeRacing.Players.update_problem(current_player, random_problem)
+    CodeRacing.Players.update_problem(current_player, Map.put(random_problem, :requested_time, DateTime.utc_now))
     render(conn, "input.json", input: random_problem.input)
   end
 
   def create(conn, %{"output" => output}) do
-    %{problem: %{output: expected_output}} = current_player = conn.assigns.current_player
-    if expected_output == output do
-      CodeRacing.Players.move_to_next_challenge(current_player)
-      render(conn, "output.json", output: "Success!!!... Try next one")
+    %{problem: %{output: expected_output, requested_time: requested_time}} = current_player = conn.assigns.current_player
+    if DateTime.diff(DateTime.utc_now, requested_time) < 10 do
+      if expected_output == output do
+        CodeRacing.Players.move_to_next_challenge(current_player)
+        render(conn, "output.json", output: "Success!!!... Try next one")
+      else
+        conn |> put_status(:bad_request) |> render("output.json", output: "Error!!!.. Try again")
+      end
     else
-      render(conn, "output.json", output: "Error!!!.. Try again")
+      conn
+      |> put_status(:bad_request)
+      |> render("output.json", output: "Time out Error!!!.. Try again")
     end
   end
 
